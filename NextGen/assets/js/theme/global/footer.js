5d355dbbe703b3f6e241195073c3a9f4eddd71b7
ElynJin <elyn.jin@silksoftware.com>
2021-10-01 22:54:20 +0800

    fix bug for footer and pdp

--- NextGen/assets/js/theme/global/footer.js
+++ NextGen/assets/js/theme/global/footer.js
@@ -1,8 +1,8 @@
 export default function(){
     console.log($('.container-mobile'));
     let actionNum = 0
-    $('.footer-container-mobile a.list-action').on("click",function(){
-        
+    $('.footer-container-mobile a.list-action').on("click",function(event){
+        event.preventDefault();
         $(this).siblings('.content').toggle();
         if(actionNum == 0){
             $(this).find('svg').html(`<use xlink:href="#icon-angle-up" />`);
