67b7e72ede659aaf2264b09ce1d1489c42ae86c5
XiaoZXixi <rudy.zeng@silksoftware.com>
2021-12-21 13:51:20 +0800

    add to cart

--- NextGen/assets/js/theme/category.js
+++ NextGen/assets/js/theme/category.js
@@ -2,7 +2,8 @@ import { hooks } from '@bigcommerce/stencil-utils';
 import CatalogPage from './catalog';
 import compareProducts from './global/compare-products';
 import FacetedSearch from './common/faceted-search';
-
+import utils from '@bigcommerce/stencil-utils';
+import modalFactory, { alertModal, showAlertModal } from './global/modal';
 export default class Category extends CatalogPage {
     onReady() {
         compareProducts(this.context.urls);
@@ -13,6 +14,7 @@ export default class Category extends CatalogPage {
             this.onSortBySubmit = this.onSortBySubmit.bind(this);
             hooks.on('sortBy-submitted', this.onSortBySubmit);
         }
+        this.cardAddToCart();
     }
 
     initFacetedSearch() {
@@ -44,4 +46,63 @@ export default class Category extends CatalogPage {
             }, 100);
         });
     }
+    // qty change
+    cardQTYChange(){
+        const $qtyDiv = $("[data-card-quantity-change]");
+        const $dec = $qtyDiv.find("[data-card-action='dec']");
+        const $inc = $qtyDiv.find("[data-card-action='inc']");
+        $dec.on("click",(event)=>{
+            event.preventDefault();
+            let value = $(event.currentTarget).siblings("input").val();
+            if(value == 0){
+                value = 0
+            }else{
+                value --
+            }
+            $(event.currentTarget).siblings("input").val(value);
+        });
+        $inc.on("click",(event)=>{
+            event.preventDefault();
+            let value = $(event.currentTarget).siblings("input").val();
+            value++;
+            $(event.currentTarget).siblings("input").val(value);
+        });
+    }
+    //add to cart
+    cardAddToCart(){
+        this.cardQTYChange();
+        const $addBtn = $("[data-card-action='add']");
+        $addBtn.on("click",(event)=>{
+            const $target = $(event.target);
+            const $cardBody = $target.parents(".card-body");
+            // const sku = $cardBody.find("[data-product-sku]").text();
+            const id = $target.attr("data-product-id");
+            const qty = $cardBody.find(".form-input--incrementTotal").val();
+            console.log(id);
+            console.log(qty);
+            const formData = new FormData();
+            formData.append('action', 'add');
+            formData.append('product_id', id);
+            formData.append('qty[]', qty);
+            if(qty > 0 ){
+
+
+                utils.api.cart.itemAdd
+                utils.api.cart.itemAdd(formData, (err, response) => {
+                    const errorMessage = err || response.data.error;
+                    if (errorMessage) {
+                        // Strip the HTML from the error message
+                        const tmp = document.createElement('DIV');
+                        tmp.innerHTML = errorMessage;
+                        return showAlertModal(tmp.textContent || tmp.innerText);
+                    }
+                    window.location.href = "/cart.php";
+                })
+            }
+            
+
+
+        });
+    }
+
 }
